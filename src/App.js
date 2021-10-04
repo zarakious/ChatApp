4dbdd42b0092cea774a43c29b05a122089d08647
zaraki <ziiedkouki@gmail.com>
2021-10-04 17:32:41 +0200

    add send messages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
-
-// using callback
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import MessageForm from "./components/MessageForm";
+import MyMessage from "./components/MyMessage";
+import TheirMessage from "./components/TheirMessage";
 
 const App = () => {
-  return <div className="message"> Hello my dad</div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="e73c8186-ce41-4b0f-84dc-94e463c60cb3"
+      userName="zarakious"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
 };
 
 export default App;
